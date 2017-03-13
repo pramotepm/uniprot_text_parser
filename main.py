a52253233c43b9cb6cd02e8bf2ab6d447016d6de
Pramote Teerasetmanakul <boatblaster@gmail.com>
2017-03-13 22:39:05 +0900

    uncomment processing lines

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import insert_varsplice
 import match_refseq_entry
 import correct_external
 
-# print "PARSING FROM SWISSPROT DATABASE"
-# create_main_schema.main()
-# print "\tcomplete"
+print "PARSE SWISSPROT DB"
+create_main_schema.main()
+print "\tcomplete"
 
-# print "INSERTING VARIANT REFERENCE FROM SWISSPROT VARSPLICE"
-# insert_varsplice.main()
-# print "\tcomplete"
+print "INSERTING VARIANT REFERENCE FROM SWISSPROT VARSPLICE"
+insert_varsplice.main()
+print "\tcomplete"
 
-# print "CORRECT THE EXTERNAL SEQUENCE"
-# correct_external.main()
-# print "\tcomplete"
+print "CORRECT THE EXTERNAL SEQUENCE"
+correct_external.main()
+print "\tcomplete"
 
-print "DISCOVERY NEW SEQUENCE IN REFSEQ PROTEIN"
+print "MAP SWISSPROT TO REFSEQ"
 match_refseq_entry.main()
 print "\tcomplete"
