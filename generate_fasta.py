940110535f4a3eabfc7d60b902ea042c42e7836b
Pramote Teerasetmanakul <boatblaster@gmail.com>
2017-03-16 22:19:11 +0900

    fixed bug

--- generate_fasta.py
+++ generate_fasta.py
@@ -10,15 +10,16 @@ def main():
     db = config.get_connection_collection()
     for doc in db.find({}, {'uniprot_id':1, 'isoform_product':1}):
         for doc2 in doc['isoform_product']:
-            if doc2['digest'] != get_digest_md5(doc2['seq']):
-                exit(1)
-            seq = Seq.Seq(doc2['seq'])
-            is_ref = 'is_ref' in doc2
-            isoform_id = '%s.%s' % (doc['uniprot_id'], doc2['isoform_id'].split('-')[1])
-            seq_record = SeqRecord(seq, isoform_id, '', '')
-            with open(os.path.join(config.get_fasta_dir_path(), ''.join([isoform_id, '.ref' if is_ref else '.var'])), 'w') as f_out:
-                SeqIO.write(seq_record, f_out, 'fasta')
-            f_out.close()
+            if 'seq' in doc2:
+                if doc2['digest'] != get_digest_md5(doc2['seq']):
+                    exit(1)
+                seq = Seq.Seq(doc2['seq'])
+                is_ref = 'is_ref' in doc2
+                isoform_id = '%s.%s' % (doc['uniprot_id'], doc2['isoform_id'].split('-')[1])
+                seq_record = SeqRecord(seq, isoform_id, '', '')
+                with open(os.path.join(config.get_fasta_dir_path(), ''.join([isoform_id, '.ref' if is_ref else '.var'])), 'w') as f_out:
+                    SeqIO.write(seq_record, f_out, 'fasta')
+                f_out.close()
 
 if __name__ == '__main__':
     main()
